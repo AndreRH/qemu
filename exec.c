88de42d37d5940c1120a3b5ef766133176f782d5
Stefan Dösinger <stefan@codeweavers.com>
2017-06-09 12:58:02 +0200

    Disable disassembler page access checks.

--- exec.c
+++ exec.c
@@ -3102,7 +3102,7 @@ MemoryRegion *get_system_io(void)
 int cpu_memory_rw_debug(CPUState *cpu, target_ulong addr,
                         uint8_t *buf, int len, int is_write)
 {
-    int l, flags;
+    int l;
     target_ulong page;
     void * p;
 
@@ -3111,20 +3111,20 @@ int cpu_memory_rw_debug(CPUState *cpu, target_ulong addr,
         l = (page + TARGET_PAGE_SIZE) - addr;
         if (l > len)
             l = len;
-        flags = page_get_flags(page);
-        if (!(flags & PAGE_VALID))
-            return -1;
+//         flags = page_get_flags(page);
+//         if (!(flags & PAGE_VALID))
+//             return -1;
         if (is_write) {
-            if (!(flags & PAGE_WRITE))
-                return -1;
+//             if (!(flags & PAGE_WRITE))
+//                 return -1;
             /* XXX: this code should not depend on lock_user */
             if (!(p = lock_user(VERIFY_WRITE, addr, l, 0)))
                 return -1;
             memcpy(p, buf, l);
             unlock_user(p, addr, l);
         } else {
-            if (!(flags & PAGE_READ))
-                return -1;
+//             if (!(flags & PAGE_READ))
+//                 return -1;
             /* XXX: this code should not depend on lock_user */
             if (!(p = lock_user(VERIFY_READ, addr, l, 1)))
                 return -1;
