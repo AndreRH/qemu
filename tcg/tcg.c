f6622371dc22a3258e18af143807c53b18e1e5ce
André Hentschel <nerv@dawncrow.de>
2017-08-12 17:24:18 +0200

    Don't build ELF code

--- tcg/tcg.c
+++ tcg/tcg.c
@@ -4270,6 +4270,7 @@ void tcg_dump_info(void)
 }
 #endif
 
+#undef ELF_HOST_MACHINE
 #ifdef ELF_HOST_MACHINE
 /* In order to use this feature, the backend needs to do three things:
 
@@ -4536,9 +4537,9 @@ static void tcg_register_jit_int(void *buf, size_t size,
 {
 }
 
-void tcg_register_jit(void *buf, size_t buf_size)
+/*void tcg_register_jit(void *buf, size_t buf_size)
 {
-}
+}*/
 #endif /* ELF_HOST_MACHINE */
 
 #if !TCG_TARGET_MAYBE_vec
